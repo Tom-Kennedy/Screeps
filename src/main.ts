39500517a9edc0107459077ebd513e729bbac389
Tom Kennedy <tom.kennedy@leiainc.com>
2020-10-24 19:42:11 -0700

    remove legacy main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { ErrorMapper } from "utils/ErrorMapper"
-import main from './old/main'
+import roomManager from 'managers/roomManager'
+import creepManager from 'managers/creepManager'
+import attackManager from 'managers/attackManager'
+import remoteMiningManager from 'managers/remoteMiningManager'
 
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
-    main()
+    creepManager.run()
+    roomManager.run()
+    remoteMiningManager.run()
+    attackManager.run()
 });
